34a907ba48923d395de23a0e0697ba238b8257f9
jianghailong <395729207@qq.com>
2016-06-06 17:08:13 +0800

    2.4.2

--- bonreeAgent.framework/Headers/bonreeAgent.h
+++ bonreeAgent.framework/Headers/bonreeAgent.h
@@ -45,8 +45,31 @@
 //设置启动概率,默认100%
 + (void)setRateOfLaunch:(double)rate;
 
+//设置config地址,默认公有云不用设置
++ (void)setConfigAddress:(NSString*)configAddress;
+
 //设置启动选项
 + (void)setOption:(int)option;
+
+//行为数据
+
+//设置下载渠道名称
++ (void)setChannelName:(NSString*)channelName;
+
+//自定义事件
++ (void)trackEvent:(NSString*)eventID name:(NSString*)eventName;
+
+/*
+ 标记页面开始和结束,这两个接口需要成对调用.
+ 在viewWillAppear或者viewDidAppear调用trackPageBegin方法
+ 在viewWillDisappear或者viewDidDisappear调用trackPageEnd方法
+ */
++ (void)trackPageBegin:(NSString *)pageId pageName:(NSString *)pageName;
++ (void)trackPageEnd:(NSString *)pageId pageName:(NSString*)pageName;
+
+
+//设置会员id
++ (void)setMemberId:(NSString *)memberId;
 
 @end
 
